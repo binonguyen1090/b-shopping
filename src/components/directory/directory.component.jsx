d7d7afba3fa9a2c6ec4b74a28629fda446df4483
Bino Nguyen <binonguyen1090@gmail.com>
2020-05-21 10:58:20 -0500

    display images

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -46,8 +46,13 @@ import './directory.style.scss'
      render(){
          return (
            <div className="directory-menu">
-             {this.state.sections.map(({ title, imageUrl, id }) => (
-               <MenuItem key={id} title={title} imageUrl={imageUrl} />
+             {this.state.sections.map(({ title, imageUrl, id, size }) => (
+               <MenuItem
+                 key={id}
+                 title={title}
+                 imageUrl={imageUrl}
+                 size={size}
+               />
              ))}
            </div>
          );
