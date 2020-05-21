ed15678a8ea6bde857afddddcceb1f17aca295e6
Bino Nguyen <binonguyen1090@gmail.com>
2020-05-21 14:41:49 -0500

    add react-router-dom and Shop component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
-      <Homepage/>
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
+      {/* <Route exec path component/> */}
     </div>
   );
 }
