75ed3c2df847a1ef3a7e46b53b16485ea1ee817b
Bino Nguyen <binonguyen1090@gmail.com>
2020-05-23 13:55:01 -0500

    add custom button style in collection item in shop page

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './collection-item.style.scss'
+import CustomButton from '../custom-button/custom-button.component'
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
   <div className="collection-item">
@@ -8,7 +9,9 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
     <div className="collection-footer">
       <span className="name">{name}</span>
       <span className="price">{price}</span>
+      
     </div>
+    <CustomButton inverted>ADD TO CART</CustomButton>
   </div>
 );
 
--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import './custom-button.style.scss'
 
-const CustomButton = ({children, isSignInGoogle, ...otherProps}) => (
-    <button 
-    className={`${isSignInGoogle ? 'google-sign-in' : ''} custom-button`} 
-                {...otherProps}
-            >
-        {children}
-    </button>
-)
+const CustomButton = ({
+  children,
+  isSignInGoogle,
+  inverted,
+  ...otherProps
+}) => (
+  <button
+    className={`${isSignInGoogle ? "google-sign-in" : ""} 
+                ${inverted ? "inverted" : ""}
+                custom-button`}
+    {...otherProps}
+  >
+    {children}
+  </button>
+);
 export default CustomButton
