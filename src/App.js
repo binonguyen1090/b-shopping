2d44149875b6a4a4b0bcd144a2f7d3e74e08bd4c
Bino Nguyen <binonguyen1090@gmail.com>
2020-05-22 20:10:45 -0500

    handle any Auth changed on Firebase

--- src/App.js
+++ src/App.js
@@ -5,19 +5,39 @@ import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/signin_signup/signin_signup.component';
+import {auth} from './firebase/firebase.utils'
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      currentUser: null
+    }
+  }
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={Homepage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndSignUpPage} />
-      </Switch>
-      {/* <Route exec path component/> */}
-    </div>
-  );
+  unSubscribeFromAuth = null
+  componentDidMount(){
+    this.unSubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+      console.log(user);
+    });
+  }
+  componentWillUnmount(){
+    this.unSubscribeFromAuth()
+  }
+  render(){
+
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Homepage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
+        </Switch>
+        {/* <Route exec path component/> */}
+      </div>
+    );
+  }
 }
 
 export default App;
