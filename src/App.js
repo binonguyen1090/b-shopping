d82550c83b449e3d3c5d90d920fa1dd131891b9c
Bino Nguyen <binonguyen1090@gmail.com>
2020-05-23 11:54:11 -0500

    redirect after login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/signin_signup/signin_signup.component';
@@ -49,14 +49,19 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={Homepage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
+          <Route exact path='/signin'
+                   render={()=> this.props.currentUser ?
+                              (<Redirect to='/' />) : 
+                              (<SignInAndSignUpPage />)} />
         </Switch>
       </div>
     );
   }
 }
-
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser
+})
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
