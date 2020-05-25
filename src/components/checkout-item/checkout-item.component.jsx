19a42107ad2caf16dae780fa1fbaecac6a00787d
Bino Nguyen <binonguyen1090@gmail.com>
2020-05-25 16:10:18 -0500

    add remove item function in checkout page

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,16 +1,30 @@
 import React from "react";
 import "./checkout-item.style.scss";
+import {connect} from 'react-redux';
+import {clearItemFromCart} from '../../redux/cart/cart.action'
 
-const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => (
-  <div className="checkout-item">
-    <div className="image-container">
-      <img src={imageUrl} alt="item" />
+
+const CheckoutItem = ({ cartItem, clearItemFromCart }) => {
+  const { name, imageUrl, price, quantity } = cartItem
+
+  return (
+    <div className="checkout-item">
+      <div className="image-container">
+        <img src={imageUrl} alt="item" />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">{quantity}</span>
+      <span className="price">{price}</span>
+      <div
+        className="remove-button"
+        onClick={() => clearItemFromCart(cartItem)}
+      >
+        &#9986;
+      </div>
     </div>
-    <span className="name">{name}</span>
-    <span className="quantity">{quantity}</span>
-    <span className="price">{price}</span>
-    <div className="remove">&#9986;</div>
-  </div>
-);
-
-export default (CheckoutItem);
+  )
+}
+const mDTP = (dispatch) => ({
+  clearItemFromCart: (item) => dispatch(clearItemFromCart(item)),
+});
+export default connect(null,mDTP)(CheckoutItem);
