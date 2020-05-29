c0827e2249fb9fca08a2bcf58512de5c40ff16e2
Bino Nguyen <binonguyen1090@gmail.com>
2020-05-28 23:49:31 -0500

    import prop to StripeCheckOut

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -5,7 +5,22 @@ const StripCheckoutButton = ({price}) => {
     const priceForStripe = price * 100
     const publishableKey = 'pk_test_mi8OGcIZ4HmQ8ClQYxB9mo8E00oxq4FceJ'
 
-    return(
-        
-    )
+    const onToken = token => {
+        console.log(token)
+        alert("pay success")
+    }
+    return (
+      <StripCheckout
+        label="Pay Now"
+        name="B SHOP INC"
+        billingAddress
+        shippingAddress
+        description={`Your total is $${price}`}
+        amount={priceForStripe}
+        panelLabel="Pay Now"
+        token={onToken}
+        stripeKey={publishableKey}
+      />
+    );
 }
+export default StripCheckoutButton
