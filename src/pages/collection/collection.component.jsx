a56740aabfe03efdc0b261caeadab782a02210b8
Bino Nguyen <binonguyen1090@gmail.com>
2020-05-28 21:31:07 -0500

    render to single page

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
-
+import {connect} from 'react-redux'
 import './collection.style.scss'
 import collectionItem from '../../components/collection-item/collection-item.component'
+import { selectCollection } from '../../redux/shop/shop.selector'
 
-const CollectionPage = ({match}) => (
-    <div className="collection">
-        {match.params.collectionId}
-    </div>
-)
 
-export default CollectionPage
+const CollectionPage = ({ collection }) => {
+  return (
+  <div className="collection-page">asd</div>
+  )
+}
+
+const mSTP = (state, ownProp) => ({
+  collection: selectCollection(ownProp.match.params.collectionId)(state)
+});
+export default connect(mSTP)(CollectionPage)
