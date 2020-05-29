44f23f8e639c4ed535debc846da8ea49b3f57796
Bino Nguyen <binonguyen1090@gmail.com>
2020-05-28 21:39:06 -0500

    style single page collections

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import './collection.style.scss'
-import collectionItem from '../../components/collection-item/collection-item.component'
+import CollectionItem from '../../components/collection-item/collection-item.component'
 import { selectCollection } from '../../redux/shop/shop.selector'
 
 
 const CollectionPage = ({ collection }) => {
+    const {title, items} = collection
+    console.log(collection)
   return (
-  <div className="collection-page">asd</div>
+  <div className="collection-page">
+      <div className="title">{title}</div>
+      <div className="items">
+          {
+              items.map(item => (
+                <CollectionItem key={item.id} item={item}/>
+              ))
+          }
+      </div>
+  </div>
   )
 }
 
