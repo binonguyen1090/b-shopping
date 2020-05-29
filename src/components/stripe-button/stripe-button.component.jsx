399f4b22da34cd7ce8c41b45adc948bc342581b2
Bino Nguyen <binonguyen1090@gmail.com>
2020-05-28 23:45:09 -0500

    Stripe Component with key and convert to Stripe cents

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import StripCheckout from 'react-stripe-checkout'
+
+const StripCheckoutButton = ({price}) => {
+    const priceForStripe = price * 100
+    const publishableKey = 'pk_test_mi8OGcIZ4HmQ8ClQYxB9mo8E00oxq4FceJ'
+
+    return(
+        
+    )
+}
